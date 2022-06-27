d5beaf747f8f797917e73f44405f85d6da9dd7cc
Catherine Siller <cathysiller503@gmail.com>
2022-06-27 16:34:38 -0500

    Update InputField.js

--- src/components/InputField.js
+++ src/components/InputField.js
@@ -1,8 +1,7 @@
-import React, { memo } from "react";
+import React from "react";
 import styled from "styled-components";
 
 const FormGroup = styled.div`
-  //height: 105px;
   margin-bottom: 16px;
   position: relative;
 `;
@@ -20,14 +19,14 @@ const Label = styled.label`
 `;
 
 const Input = styled.input`
+  border: 1px solid #898E9A;
   margin-bottom: 8px;
   padding: 16px 12px;
   width: -webkit-fill-available;
-  //width: 100%;
 
-  :error {
-    border-color: #F54545;
-  }
+  ${({ error }) => error && `
+    border: 2px solid #F54545;
+  `}
 `;
 
 const Error = styled.p`
@@ -43,7 +42,6 @@ const Error = styled.p`
   margin: 0 0;
 `;
 
-
 const InputField = ({ 
     error,
     id,
@@ -55,14 +53,13 @@ const InputField = ({
     value,
   }) => {
 
-  //const [input, setInput] = useState();
-
   return (
     <FormGroup>
       <Label>
         {label}
       </Label>
       <Input
+        error={error}
         id={id}
         onChange={onChange}
         placeholder={placeholder}
@@ -76,66 +73,3 @@ const InputField = ({
 };
 
 export default InputField;
-
-
-/*
-import React, { useState } from "react";
-import styled from 'styled-components';
-
-const Label = styled.label`
-  align-items: center;
-  color: #141E35;
-  display: flex;
-  font-family: Inter,sans-serif;
-  font-size: 14px;
-  font-style: normal;
-  font-weight: 600;
-  letter-spacing: -0.01em;
-  line-height: 24px;
-`
-
-const Input = styled.input`
-  margin-bottom: 8px;
-  padding: 16px 12px;
-  width: 100%;
-  
-  /*
-  &[type="email"] {
-    background-color: yellow;
-    margin-bottom: 16px;
-  }
-`;
-
-const Error = styled.p`
-  align-items: center;
-  color: #F54545;
-  display: flex;
-  font-family: 'JetBrains Mono';
-  font-size: 12px;
-  font-style: normal;
-  font-weight: 400;
-  letter-spacing: -0.01em;
-  line-height: 20px;
-`
-
-const InputField = ({ value, label, name, placeholder, type, onChange, error }) => {
-  const [input, setInput] = useState();
-
-  return (
-    <div className="form-group">
-      {label && <Label htmlFor="input-field">{label}</Label>}
-      <Input
-        className="form-control"
-        name={name}
-        onChange={(e) => setInput(e.target.value)}
-        placeholder={placeholder}
-        type={type}
-        value={input}
-      />
-      {/*error && <Error htmlFor="input-field">{error}</Error>}
-    </div>
-  )
-};
-
-export default InputField;
-*/
