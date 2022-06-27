2fa79b128be09d346b877ed374bb1552071a6e79
Catherine Siller <c-catherine.siller@MACC02FH28VMD6R.(none)>
2022-06-27 16:01:03 -0500

    removed commented text

--- src/components/InputField.js
+++ src/components/InputField.js
@@ -41,7 +41,6 @@ const Error = styled.p`
   margin: 0 0;
 `;
 
-
 const InputField = ({ 
     error,
     id,
@@ -72,66 +71,3 @@ const InputField = ({
 };
 
 export default InputField;
-
-
-/*
-import React, { useState } from "react";
-import styled from 'styled-components';
-
-const Label = styled.label`
-  align-items: center;
-  color: #141E35;
-  display: flex;
-  font-family: Inter,sans-serif;
-  font-size: 14px;
-  font-style: normal;
-  font-weight: 600;
-  letter-spacing: -0.01em;
-  line-height: 24px;
-`
-
-const Input = styled.input`
-  margin-bottom: 8px;
-  padding: 16px 12px;
-  width: 100%;
-  
-  /*
-  &[type="email"] {
-    background-color: yellow;
-    margin-bottom: 16px;
-  }
-`;
-
-const Error = styled.p`
-  align-items: center;
-  color: #F54545;
-  display: flex;
-  font-family: 'JetBrains Mono';
-  font-size: 12px;
-  font-style: normal;
-  font-weight: 400;
-  letter-spacing: -0.01em;
-  line-height: 20px;
-`
-
-const InputField = ({ value, label, name, placeholder, type, onChange, error }) => {
-  const [input, setInput] = useState();
-
-  return (
-    <div className="form-group">
-      {label && <Label htmlFor="input-field">{label}</Label>}
-      <Input
-        className="form-control"
-        name={name}
-        onChange={(e) => setInput(e.target.value)}
-        placeholder={placeholder}
-        type={type}
-        value={input}
-      />
-      {/*error && <Error htmlFor="input-field">{error}</Error>}
-    </div>
-  )
-};
-
-export default InputField;
-*/
